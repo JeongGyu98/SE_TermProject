c93d30da6f17d80b82396a34e70ca50d85d3cd0c
rhewj <fbckdals99@gmail.com>
2021-05-20 19:11:10 +0900

    Login 버튼 아래로 고정

--- app/src/main/java/com/example/se_termproject/LoginActivity.java
+++ app/src/main/java/com/example/se_termproject/LoginActivity.java
@@ -48,7 +48,8 @@ public class LoginActivity extends AppCompatActivity {
 
         // [START config_signin]
         // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        GoogleSignInOptions gso =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
